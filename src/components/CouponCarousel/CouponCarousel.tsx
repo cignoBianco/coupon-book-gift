55bdd0d4e3d4abade81394276c18bf07534a07f3
smiana123@gmail.com <smiana123@gmail.com>
2026-01-31 21:47:40 +0300

    fix carousel sliding

--- src/components/CouponCarousel/CouponCarousel.tsx
+++ src/components/CouponCarousel/CouponCarousel.tsx
@@ -14,10 +14,10 @@ export function CouponCarousel() {
     const onTouchEnd = (e: React.TouchEvent<HTMLDivElement>) => {
         const diff = startX.current - e.changedTouches[0].clientX;
         if (diff > 50 && current < coupons.length - 1) {
-            setCurrent(current + 1);
+            setCurrent(c => c + 1);
         }
         if (diff < -50 && current > 0) {
-            setCurrent(current - 1);
+            setCurrent(c => c - 1);
         }
     };
 
@@ -29,7 +29,7 @@ export function CouponCarousel() {
         >
             <div
                 className="carousel-track"
-                style={{ transform: `translateX(-${current * 100}%)` }}
+                style={{ transform: `translateX(calc(-${current} * (100vw - 40px) - ${current} * 20px))` }}
             >
                 {coupons.map((c, i) => (
                     <CouponCard
